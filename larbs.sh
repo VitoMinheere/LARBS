40f71126315a825a11e9fbe495cb2bd1e3dc8336
Your Name <you@example.com>
2021-01-02 23:25:20 +0100

    use merge_both branch

--- larbs.sh
+++ larbs.sh
@@ -14,10 +14,10 @@ while getopts ":a:r:b:p:h" o; do case "${o}" in
 	*) printf "Invalid option: -%s\\n" "$OPTARG" && exit 1 ;;
 esac done
 
-[ -z "$dotfilesrepo" ] && dotfilesrepo="https://github.com/lukesmithxyz/voidrice.git"
+[ -z "$dotfilesrepo" ] && dotfilesrepo="https://github.com/VitoMinheere/voidrice.git"
 [ -z "$progsfile" ] && progsfile="https://raw.githubusercontent.com/LukeSmithxyz/LARBS/master/progs.csv"
 [ -z "$aurhelper" ] && aurhelper="yay"
-[ -z "$repobranch" ] && repobranch="master"
+[ -z "$repobranch" ] && repobranch="merge_both"
 
 ### FUNCTIONS ###
 
